b717b36b094e2f8dcf2245e85913583d35a6c579
Aayush Pandey <aayushpandey1100@gmail.com>
2023-11-02 23:26:21 +0530

    crud done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express=require("express");
 
 const app=express()
+
+app.use(express.json());
+
 const dbConfig=require('./db/db')
+
+const roomsRoute=require("./routes/roomsRoute")
+const usersRoute=require("./routes/usersRoute")
+const bookingsRoute=require("./routes/bookingsRoute")
+
+
+app.use("/api/rooms",roomsRoute)
+app.use("/api",usersRoute)
+app.use("/api",bookingsRoute)
+
 
 const port=process.env.PORT||5000;
 
 app.listen(port,()=>{
     console.log("server is running on port: ",port)
 })
+
